40130089023e6a3217ccfbfef24e4ddacdd13d0b
Kavyasri <25tq1a6696@siddhartha.org.in>
2026-09-10 07:29:48 -1200

    Fix merge migration revision length

--- migrations/versions/012_merge_heads.py
+++ migrations/versions/012_merge_heads.py
@@ -1,6 +1,6 @@
 """merge stock alerts and payment migration heads
 
-Revision ID: 012_merge_stock_alerts_and_payment_heads
+Revision ID: 012_merge_heads
 Revises: 011_add_stock_alerts, 5b18a23d91c1
 Create Date: 2026-09-09 22:49:52.507347
 
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '012_merge_stock_alerts_and_payment_heads'
-down_revision = ('011_add_stock_alerts', '5b18a23d91c1')
+revision = "012_merge_heads"
+down_revision = ("011_add_stock_alerts", "5b18a23d91c1")
 branch_labels = None
 depends_on = None
 
